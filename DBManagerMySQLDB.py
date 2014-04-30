f64befd93af5e537ff4eb3e6219f6cc700cbce68
Codenginebd <codenginebd@gmail.com>
2014-05-01 01:04:54 +0600

    Replaced MySQLDB by pymysql

--- DBManagerMySQLDB.py
+++ DBManagerMySQLDB.py
@@ -1,6 +1,6 @@
 # - * - coding: UTF-8 - * -
 from datetime import datetime
-import pymysql
+import MySQLdb
 from FileHandler import *
 import urllib2
 
@@ -12,11 +12,9 @@ host = '127.0.0.1'
 class DBWraper:
     def __init__(self):
         try:
-            self.dbconn = conn = pymysql.connect(charset='utf8', init_command='SET NAMES UTF8',host=host, port=3306, user=uname, passwd=password, db=db_name)
-            #self.dbconn.set_character_set('utf8') #cur = self.dbconn.cursor()
+            self.dbconn = MySQLdb.connect(charset='utf8', init_command='SET NAMES UTF8',host=host,user=uname,passwd=password,db=db_name)
+            self.dbconn.set_character_set('utf8') #cur = self.dbconn.cursor()
             #cur.execute('SET NAMES utf8;')
-            print 'Database connection successfull.'
-            #print self.dbconn
         except Exception,msg:
             self.dbconn = None
 
@@ -27,9 +25,8 @@ class DBWraper:
                 for each_data in data:
                     try:
                         cursor = self.dbconn.cursor()
-                        query = "insert into lead_details(full_name,name2,name3,a1,a2,tel,fax,email,web,last_update) values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (pymysql.escape_string(each_data.get('full_name')),pymysql.escape_string(each_data.get('name2')),pymysql.escape_string(each_data.get('name3')),pymysql.escape_string(each_data.get('address1')),pymysql.escape_string(each_data.get('address2')),pymysql.escape_string(each_data.get('tel')),pymysql.escape_string(each_data.get('fax')),pymysql.escape_string(each_data.get('email')),pymysql.escape_string(each_data.get('web')),str(datetime.now()))
+                        query = "insert into lead_details(full_name,name2,name3,a1,a2,tel,fax,email,web,last_update) values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (MySQLdb.escape_string(each_data.get('full_name')),MySQLdb.escape_string(each_data.get('name2')),MySQLdb.escape_string(each_data.get('name3')),MySQLdb.escape_string(each_data.get('address1')),MySQLdb.escape_string(each_data.get('address2')),MySQLdb.escape_string(each_data.get('tel')),MySQLdb.escape_string(each_data.get('fax')),MySQLdb.escape_string(each_data.get('email')),MySQLdb.escape_string(each_data.get('web')),str(datetime.now()))
                         cursor.execute(query)
-                        cursor.close()
                     except Exception,msg:
                         print 'Exception Occured Inside init_db'
                         print msg
@@ -43,7 +40,6 @@ class DBWraper:
                 cursor.execute(query)
                 query = 'insert into last_crawled_state(lead_id) values(%s)' % str(lead_id)
                 cursor.execute(query)
-                cursor.close()
 
     def update_lead_info(self,lead_info_row):
         if self.dbconn:
@@ -53,7 +49,6 @@ class DBWraper:
                     cursor = self.dbconn.cursor()
                     query = "update lead_details set full_name='%s',name2='%s',name3='%s',a1='%s',a2='%s',tel='%s',fax='%s',email='%s',web='%s',last_update='%s' where id=%s" % (urllib2.quote(lead_info_row[1].encode('utf8')),urllib2.quote(lead_info_row[2].encode('utf8')),urllib2.quote(lead_info_row[3].encode('utf8')),urllib2.quote(lead_info_row[4].encode('utf8')),urllib2.quote(lead_info_row[5].encode('utf8')),lead_info_row[6],lead_info_row[7],lead_info_row[8],lead_info_row[9],datetime.now(),str(lead_info_row[0]))
                     cursor.execute(query)
-                    cursor.close()
                 except Exception,msg:
                     print 'Exception Occured Inside Update Lead Info'
                     print msg
@@ -64,9 +59,7 @@ class DBWraper:
             query = 'select * from last_crawled_state'
             cursor = self.dbconn.cursor()
             cursor.execute(query)
-            data = cursor.fetchone()
-            cursor.close()
-            return data
+            return cursor.fetchone()
 
     def read_lead_info(self,start=0,count=20):
         results = []
@@ -81,7 +74,6 @@ class DBWraper:
             rows = cursor.fetchall()
             for row in rows:
                 results += [row]
-            cursor.close()
         return results
 
     def read_lead_info_all(self):
@@ -97,7 +89,6 @@ class DBWraper:
             rows = cursor.fetchall()
             for row in rows:
                 results += [row]
-            cursor.close()
         return results
 
     def reset_db(self):
@@ -107,7 +98,6 @@ class DBWraper:
                     query = 'delete from lead_details;delete from last_crawled_state;alter table lead_details auto_increment=1;'
                     cursor = self.dbconn.cursor()
                     cursor.execute(query)
-                    cursor.close()
             except Exception,msg:
                 pass
 
@@ -116,4 +106,4 @@ class DBWraper:
         if self.dbconn:
             self.dbconn.close()
 
-#DBWraper()
+#DBWraper().init_db()
